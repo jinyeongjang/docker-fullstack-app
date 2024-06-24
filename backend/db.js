ce6f489ffe031c59ecc8ca8bd7717ef359070400
wlsdud0448 <wlsdud0448@naver.com>
2024-06-24 16:20:39 +0900

    changed

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,20 @@
 const mysql = require('mysql');
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: '12341234!@',
-    database: 'myapp',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Database connection failed:', err);
+    } else {
+        console.log('Connected to MySQL database!');
+        connection.release(); // 연결 해제
+    }
 });
 
 exports.pool = pool;
