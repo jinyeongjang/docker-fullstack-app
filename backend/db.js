090e102ed5bbfcc880c35e3d8af08a7f53e624c1
wlsdud0448 <wlsdud0448@naver.com>
2024-06-24 11:27:31 +0900

    first commit

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+// backend/db.js
+
+const mysql = require("mysql");
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'mysql',
+    user: 'root',
+    password: '12341234!@',
+    database: 'myapp',
+});
+
+exports.pool = pool;
